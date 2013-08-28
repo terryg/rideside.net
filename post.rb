c4e5527fbe8de97173bed61c46999905834ea2b7
Terry G Lorber 2 <tgl@rideside.net>
2013-08-27 21:14:58 -0400

    ENH fixup img display

--- post.rb
+++ post.rb
@@ -8,7 +8,16 @@ class Post
   attr_accessor :body
 
   def initialize(attr)
-    if attr['type'] == 'text'
+    if attr['type'] == 'photo'
+      self.blog_name = attr['blog_name']
+      self.date = attr['date']
+      self.post_url = attr['post_url']
+      self.title = ""
+      self.description = ""
+
+      link = attr['photos'][0]['original_size']['url']
+      self.body = "<img src=\"#{link}\"/><br/><strong>#{attr['caption']}</strong>"
+    else
       self.blog_name = attr['blog_name']
       self.date = attr['date']
       self.post_url = attr['post_url']
@@ -16,18 +25,6 @@ class Post
       self.description = attr['description']
       self.body = attr['body']
 
-    elsif attr['type'] == 'photo'
-      self.blog_name = attr['blog_name']
-      self.date = attr['date']
-      self.post_url = attr['post_url']
-      self.title = ""
-      self.description = ""
-
-      link = attr['image_permalink']
-      self.body = "<img src=\"#{link}\"/><br/><strong>#{attr['caption']}</strong>"
-
     end
   end
-
-  
 end
