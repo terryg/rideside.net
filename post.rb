77c6f0786cb0092a291162ecb507629e513febe6
Terry G Lorber 2 <tgl@rideside.net>
2013-08-31 22:05:48 -0400

    ENH remove duplication

--- post.rb
+++ post.rb
@@ -8,45 +8,33 @@ class Post
   attr_accessor :body
 
   def initialize(attr)
+    self.blog_name = attr['blog_name']
+    self.date = attr['date']
+    self.post_url = attr['post_url']
+
     if attr['type'] == 'photo'
-      self.blog_name = attr['blog_name']
-      self.date = attr['date']
-      self.post_url = attr['post_url']
       self.title = ""
       self.description = ""
 
       link = attr['photos'][0]['original_size']['url']
       self.body = "<img src=\"#{link}\"/><br/><strong>#{attr['caption']}</strong>"
     elsif attr['type'] == 'video'
-      self.blog_name = attr['blog_name']
-      self.date = attr['date']
-      self.post_url = attr['post_url']
       self.title = attr['title']
       self.description = attr['description']
       self.body = attr['player'][0]['embed_code']
 
     elsif attr['type'] == 'text'
-      self.blog_name = attr['blog_name']
-      self.date = attr['date']
-      self.post_url = attr['post_url']
       self.title = attr['title']
       self.description = attr['description']
       self.body = attr['body']
 
     elsif attr['type'] == 'link'
-      self.blog_name = attr['blog_name']
-      self.date = attr['date']
-      self.post_url = attr['post_url']
       self.title = attr['title']
       self.description = attr['description']
       self.body = "<a href=\"#{attr['url']}\" _target=\"blank\">&gt;&gt;</a>"
 
     else
       puts "*** #{attr}"
-
-      self.blog_name = attr['blog_name']
-      self.date = attr['date']
-      self.post_url = attr['post_url']
       self.title = attr['title']
       self.description = attr['description']
       self.body = attr['body']
