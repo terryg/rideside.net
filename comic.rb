c13178a6e9b6b59321b65c4dc4c9bcc738a58578
Terry G Lorber 2 <tgl@rideside.net>
2013-12-31 08:10:09 -0500

    ENH add XKCD

--- comic.rb
+++ comic.rb
@@ -5,11 +5,34 @@ class Comic
       @comics = []
       text = File.open('comics.txt').read
       text.each_line do |line|
-        @comics << line.split(",")
+        c = line.split(",")
+        @comics << [c[0].strip, c[1].strip]
       end
     end
 
     @comics
+  end
+
+  def self.xkcd
+    url = "http://xkcd.com"
+
+    output = "<div><strong>XKCD</strong></div>"
+    output << "<div>Searching... <a href=\"#{url}\">#{url}</a></div>"
+
+    encoded_url = URI.encode(url)
+    uri = URI.parse(encoded_url)
+    response = Net::HTTP.get_response(uri)
+
+    if response.code == "200"
+      lines = response.body.split(/\n/)
+      lines.each do |l|
+        output << "<div>#{l}</div>" if l.match(/imgs.xkcd.com\/comics/)
+      end
+    else
+      output << "<div>Response code is #{response.code}</div>"
+    end
+
+    output
   end
 
   def self.display(title, path)
