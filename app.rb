b6813f3cb382fcb2ecaf3c41976847dcc3504bfb
Terry G Lorber <tgl@rideside.net>
2015-12-10 22:02:28 -0500

    Post Italian Cookie recipe.

--- app.rb
+++ app.rb
@@ -13,19 +13,19 @@ require './models/node'
 
 Thread.new do 
   while true do
-	  uri = URI('https://raw.githubusercontent.com/terryg/resume/master/README.md')
+    uri = URI('https://raw.githubusercontent.com/terryg/resume/master/README.md')
     response = Net::HTTP.get(uri)   
-  
-	  File.open('views/resume.haml', 'w') { |file| 
-			file.write(".content\n")
-			file.write("  :markdown\n")
-			response.each_line { |line|
-				file.write("    ")				
-				file.write(line)
-  		}	
-		}
+    
+    File.open('views/resume.haml', 'w') { |file| 
+      file.write(".content\n")
+      file.write("  :markdown\n")
+      response.each_line { |line|
+        file.write("    ")				
+        file.write(line)
+      }	
+    }
 
-	  sleep(60.minutes)
+    sleep(60.minutes)
   end
 end
 
@@ -53,65 +53,65 @@ class App < Sinatra::Base
     haml :comics
   end
 
-	get '/tracker' do
-		if !params[:q].nil? 
-			@years = []
+  get '/tracker' do
+    if !params[:q].nil? 
+      @years = []
 
-			revs = NodeRevision.all(:body.like => "%#{params[:q]}%")
+      revs = NodeRevision.all(:body.like => "%#{params[:q]}%")
       comments = Comment.all(:comment.like => "%#{params[:q]}%")
 
-     	@nodes = []
+      @nodes = []
 
       revs.each do |r| 
-				@nodes << r.node
+        @nodes << r.node
       end
 
-	    comments.each do |c| 
-				@nodes << c.node
+      comments.each do |c| 
+        @nodes << c.node
       end
-		else
-  		min = Time.at(Node.first().created).strftime("%Y")
-	  	max = Time.at(Node.last().created).strftime("%Y")
-		  @years = (min.to_i..max.to_i).step(1)
-		  @nodes = []      
-		end
+    else
+      min = Time.at(Node.first().created).strftime("%Y")
+      max = Time.at(Node.last().created).strftime("%Y")
+      @years = (min.to_i..max.to_i).step(1)
+      @nodes = []      
+    end
 
-		@months = []
+    @months = []
 
-		
-		haml :tracker
+    
+    haml :tracker
   end
 
-	get '/tracker/:year' do
-		@year = params[:year]
+  get '/tracker/:year' do
+    @year = params[:year]
     @years = []	
-		@months = ("01".."12")
-		@nodes = []
-		haml :tracker
-	end
+    @months = ("01".."12")
+    @nodes = []
+    haml :tracker
+  end
 
-	get '/tracker/:year/:month' do
-		@year = params[:year]
+  get '/tracker/:year/:month' do
+    @year = params[:year]
     @month = params[:month]
 
-		year = @year.to_i
-		month = @month.to_i
+    year = @year.to_i
+    month = @month.to_i
 
-		pos = Date.new(year, month, 1).to_time.to_i
+    pos = Date.new(year, month, 1).to_time.to_i
 
     if month == 12
-			year = year + 12
+      year = year + 12
       month = 1
-		else
-			month = month + 1
-		end
+    else
+      month = month + 1
+    end
 
-		npos = Date.new(year, month, 1).to_time.to_i
+    npos = Date.new(year, month, 1).to_time.to_i
 
-		@nodes = Node.all(:created.gt => pos, :created.lt => npos)
-		@years = []
+    @nodes = Node.all(:created.gt => pos, :created.lt => npos)
+    @years = []
     @months = [] 
-		haml :tracker
+    haml :tracker
   end
 
   get '/' do
@@ -136,17 +136,21 @@ class App < Sinatra::Base
     haml :index
   end
 
-	get '/node/:id' do
-		node = Node.first(:id => params[:id])
-		created = Time.at(node.created)
-		@year = created.year
-		@month = created.month
-		@node_rev = node.node_revisions.last unless node.nil?
-	  haml :node unless @node_rev.nil?  
-	end
+  get '/node/:id' do
+    node = Node.first(:id => params[:id])
+    created = Time.at(node.created)
+    @year = created.year
+    @month = created.month
+    @node_rev = node.node_revisions.last unless node.nil?
+    haml :node unless @node_rev.nil?  
+  end
 
   get '/~tgl' do
     haml :resume, :layout => false
+  end
+
+  get '/~tgl/italian_cookies' do
+    haml :italian_cookies, :layout => false
   end
 
 end
