17ca490199a9d615f8c08b7ef9c2ca2b6d34c93d
Terry G Lorber <tgl@rideside.net>
2017-02-18 06:10:05 +0000

    Use app client to retrieve timeline

--- app.rb
+++ app.rb
@@ -103,8 +103,15 @@ class App < Sinatra::Base
   get '/' do
     @posts = []
 
-    client = Tumblr::Client.new
+    Tumblr.configure do |config|
+      config.consumer_key = ENV['TUMBLR_CONSUMER_KEY']
+      config.consumer_secret = ENV['TUMBLR_CONSUMER_SECRET']
+      config.oauth_token = ENV['TUMBLR_TOKEN']
+      config.oauth_token_secret = ENV['TUMBLR_TOKEN_SECRET']
+    end
 
+    client = Tumblr::Client.new
+    
     users = User.get_all
 
     users.each do |user|
