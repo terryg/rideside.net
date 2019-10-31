10a5256d6d145a402c5baa03661c92869143b1d1
Terry Lorber <terry@erosapp.com>
2019-10-31 10:38:20 -0400

    Don't redirect, do it outside of the app.

--- app.rb
+++ app.rb
@@ -23,12 +23,6 @@ class App < Sinatra::Base
   end
   
   before do
-    puts request.env['REQUEST_URI']
-    puts request.env['SERVER_INFO']
-    puts request.env['PATH_INFO']
-    if request.env['HTTP_HOST'].match(/herokuapp\.com/)
-      redirect 'http://www.rideside.net', 301
-    end
     current_user
   end
 
