cc4433f9ca211d3d1207c773ef477c67f1a46221
Terry Lorber <tgl@rideside.net>
2025-02-20 13:37:41 -0500

    deploy with ansible

---
+++ deploy/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+BRANCH=$1
+
+if [ -z "${BRANCH}" ]; then
+  echo "usage: ./build.sh BRANCH"
+  exit 1
+fi
+
+HERE=$( cd $( dirname "${BASH_SOURCE[0]}" ) >/dev/null 2>&1 && pwd )
+TOPDIR=$( dirname ${HERE} )
+
+TMP_DIR=/tmp/rideside.net
+rm -rf $TMP_DIR
+mkdir -p $TMP_DIR
+
+BUILD_DIR=${TMP_DIR}/${BRANCH}
+DEPLOYMENT_DIR=${TMP_DIR}/deployment
+
+cleanup() {
+  rm -rfv $BUILD_DIR
+}
+
+trap "cleanup" exit
+
+if [ "${BRANCH}" = "local" ]; then
+  ln -sf ${TOPDIR} ${BUILD_DIR}
+else
+  git clone https://github.com/terryg/rideside.net.git $BUILD_DIR
+  cd $BUILD_DIR
+  git checkout $BRANCH
+fi
+
+cd $BUILD_DIR
+
+rm -rf $DEPLOYMENT_DIR
+mkdir -p $DEPLOYMENT_DIR
+
+git archive --prefix=rideside/ --format=tar HEAD | gzip >$DEPLOYMENT_DIR/rideside.tar.gz
+
+
+
